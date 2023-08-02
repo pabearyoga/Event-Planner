93b4f0ecb75c82e339cb3be38bfdaa97f071b581
Pabearyoga <pabearyoga@gmail.com>
2023-08-02 13:54:32 +0300

    fix App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from './components/Layout/Layout';
 
-// import { Home } from './pages/Home/Home';
+import { Home } from './pages/Home/Home';
+// import { CreateEvent } from './pages/CreateEvent/CreateEvent';
+// import { EventDetails } from './pages/EventDetails/EventDetails';
+// import { EditEvent } from './pages/EditEvent/EditEvent';
 
-const Home = lazy(() => import('./pages/Home/Home'));
-const CreateEvent = lazy(() => import('./pages/CreateEvent/CreateEvent'));
-const EventDetails = lazy(() => import('./pages/EventDetails/EventDetails'));
-const EditEvent = lazy(() => import('./pages/EditEvent/EditEvent'));
+// const Home = lazy(() => import('./pages/Home/Home'));
+// const CreateEvent = lazy(() => import('./pages/CreateEvent/CreateEvent'));
+// const EventDetails = lazy(() => import('./pages/EventDetails/EventDetails'));
+// const EditEvent = lazy(() => import('./pages/EditEvent/EditEvent'));
 
 function App() {
   return (
-    // <Layout>
-    //   <div>app</div>
-    //   <Home></Home>
-    // </Layout>
+    <Layout>
+      <Home></Home>
+      {/* <CreateEvent></CreateEvent>
+      <EventDetails></EventDetails>
+      <EditEvent></EditEvent> */}
+    </Layout>
 
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="create" element={<CreateEvent />} />
-        <Route path="event/:id" element={<EventDetails />} />
-        <Route path="edit" element={<EditEvent />} />
-        {/* 404 Not Found Page with delayed redirect */}
-        {/* <Route
-          path="*"
-          element={<NotFoundPage component={NotFound} redirectTo="/" />}
-        /> */}
-      </Route>
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index element={<Home />} />
+    //       <Route path='create' element={<CreateEvent />} />
+    //       <Route path='event/:id' element={<EventDetails />} />
+    //       <Route path='edit' element={<EditEvent />} />
+    //     {/* 404 Not Found Page with delayed redirect */}
+    //     <Route path="*" element={
+    //       <NotFoundPage component={NotFound} redirectTo="/" />
+    //     } />
+    //   </Route>
+    // </Routes>
   );
 }
 
