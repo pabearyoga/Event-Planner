065d75ffee94be9044fa7bf1b065a353957899c5
Pabearyoga <pabearyoga@gmail.com>
2023-08-02 13:54:09 +0300

    add Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,20 @@
 import css from './Home.module.css';
+import Category from '../../components/Category/Category';
+import SortBy from '../../components/SortBy/SortBy';
+import { Btn } from '../../components/Btn/Btn';
+import Title from '../../components/Title/Title'
+
 
 
 export const Home = () => {
     return (
         <div className={css.home}>
-            Home
+            <Title>My events</Title>
+            <div className={css.settings}>
+                    <Category></Category>
+                    <SortBy></SortBy>
+                    <Btn>Add new event</Btn>
+            </div>
         </div>
     )
 };
