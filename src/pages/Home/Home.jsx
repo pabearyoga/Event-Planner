5b2870a26d90b14ecf3135516b2f4a2e81d048a3
Pabearyoga <pabearyoga@gmail.com>
2023-09-21 22:23:26 +0300

    add FilterSelect styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import css from './Home.module.css';
-import Category from '../../components/Category/Category';
-import SortBy from '../../components/SortBy/SortBy';
+import FilterSelect from '../../components/FilterSelect/FilterSelect';
 import { Btn } from '../../components/Btn/Btn';
 import Title from '../../components/Title/Title'
 import EventCardList from '../../components/EventCardList/EventCardList';
 
 import { getAllEvents } from '../../utils/services/eventService';
 
-
 import { NavLink } from 'react-router-dom';
+
+import {CiFilter} from 'react-icons/ci'
+import {LuSettings2} from 'react-icons/lu'
+
 
 
 
 const Home = () => {
 
     const [events, setEvents] = useState([]);
+    const [category, setCategory] = useState('Category')
+    const [sortBy, setSortBy] = useState('Sort by')
+    const [showCategoryOption, setShowCategoryOption] = useState(false)
+    const [showSortByOption, setShowSortByOption] = useState(false)
+
+    const categoryList = ['All', 'Art', 'Music', 'Business', 'Conference', 'Workshop', 'Party', 'Sport', 'Other'] 
+    const sortByList = ['by name', 'by name', 'by data', 'by data', 'by priority', 'by priority']
 
     useEffect(() => {
         const fetchEvents = async () => {
@@ -30,13 +39,36 @@ const Home = () => {
         fetchEvents();
     }, []); 
 
+    const categorySelectClick = () => {
+        setShowCategoryOption(prevShowCategoryOption => !prevShowCategoryOption)
+    }
+
+    const sortBySelectClick = () => {
+        setShowSortByOption(prevShowSortByOption => !prevShowSortByOption)
+    }
+
+
     return (
         <div className={css.home}>
             <div className={css.homeHeader}>
 
                 <div className={css.settings}>
-                    <Category></Category>
-                    <SortBy></SortBy>
+                    <FilterSelect
+                        selectValue={category}
+                        handleSelectClick={categorySelectClick}
+                        Icon={CiFilter}
+                        showOption={showCategoryOption}
+                        optionList={categoryList}
+                        handleOptionSelect={(e)=> setCategory(e.target.name)}
+                    ></FilterSelect>
+                    <FilterSelect
+                        selectValue={sortBy}
+                        handleSelectClick={sortBySelectClick}
+                        Icon={LuSettings2}
+                        showOption={showSortByOption}
+                        optionList={sortByList}
+                        handleOptionSelect={(e)=> setSortBy(e.target.name)}
+                    ></FilterSelect>
                     <NavLink to="create">
                         <Btn>Add new event</Btn>
                     </NavLink>
